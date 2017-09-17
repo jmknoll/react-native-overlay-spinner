5ac2c7dcea3eca30f9d579a7b5ed61daf1a461ba
Jameson Knoll <j.knoll621@gmail.com>
2017-09-17 17:26:09 +0800

    control color, size, and background via props

--- src/index.js
+++ src/index.js
@@ -11,17 +11,33 @@ import {
 
 export default class LoadingSpinner extends Component {
   render() {
+    const { color, size, backgroundColor } = this.props
     return(
-      <View style={styles.container}>
+      <View style={{
+        flex: 1,
+        backgroundColor: backgroundColor,
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
         <ActivityIndicator
-          color={'white'}
-          size={'large'}
+          color={color}
+          size={size}
           style={{ flex: 1 }}
         />
         <Text>{this.props.loadingText}</Text>
       </View>
     )
   }
+}
+
+LoadingSpinner.defaultProps = {
+  color: 'white',
+  size: 'large'
 }
 
 const styles = StyleSheet.create({
