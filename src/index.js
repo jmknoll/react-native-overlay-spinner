c6c1656d9dbf413515e1801b31608c9df2a6edea
Jameson Knoll <j.knoll621@gmail.com>
2017-09-17 17:26:42 +0800

    add default props to preserve backwards compatibility

--- src/index.js
+++ src/index.js
@@ -37,19 +37,6 @@ export default class LoadingSpinner extends Component {
 
 LoadingSpinner.defaultProps = {
   color: 'white',
-  size: 'large'
+  size: 'large',
+  backgroundColor: 'rgba(0, 0, 0, 0.25)'
 }
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'rgba(0, 0, 0, 0.25)',
-      position: 'absolute',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-  })
